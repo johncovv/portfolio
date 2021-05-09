364fb3e373a08bc7e7f77224005b85fb137dbe1c
John Covv <johncovv@outlook.com>
2021-05-09 15:23:26 -0300

    Updating skills data

--- src/assets/objects/skills.ts
+++ src/assets/objects/skills.ts
@@ -1,31 +1,30 @@
-import { FaHtml5, FaSass, FaNodeJs, FaReact, FaAngular } from 'react-icons/fa';
+import {
+	FaHtml5,
+	FaSass,
+	FaNodeJs,
+	FaReact,
+	FaAngular,
+	FaLess,
+} from 'react-icons/fa';
 import { IoLogoGithub } from 'react-icons/io';
-import { SiJavascript } from 'react-icons/si';
+import { SiJavascript, SiTypescript, SiGit } from 'react-icons/si';
 
 export default [
-	{
-		name: 'ReactJs',
-		color: '#61DBFB',
-		url: 'https://github.com/topics/react',
-		Icon: FaReact,
-		description:
-			'O React é uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário em páginas web. É mantido pelo Facebook, Instagram, outras empresas e uma comunidade de desenvolvedores individuais.\nUtilizado em sites como Facebook, Netflix, Dropbox, Atlassian, Airbnb, Reddit, BBC, Cloudflare e outros.',
-	},
 	{
 		name: 'Angular',
 		color: '#dd0031',
 		url: 'https://github.com/topics/angular',
 		Icon: FaAngular,
 		description:
-			'É um framework JavaScript de código aberto mantido pela Google para a construção de SPA (Single Page Application)',
+			'Angular (criado pelo Google), é uma estrutura de design de aplicativo e plataforma de desenvolvimento para a criação de aplicativos SPA (Single-Page Application) eficientes e sofisticados.\n\nMantida pelo Google e uma enorme comunidade de desenvolvedores individuais.',
 	},
 	{
-		name: 'Github',
-		color: '#FFFFFF',
-		url: 'https://github.com/',
-		Icon: IoLogoGithub,
+		name: 'ReactJs',
+		color: '#61DBFB',
+		url: 'https://github.com/topics/react',
+		Icon: FaReact,
 		description:
-			'Maior plataforma de hospedagem e colaboracão de código utilizando versionamento Git. ',
+			'React é uma biblioteca JavaScript de código aberto com foco em criação de interfaces SPA.',
 	},
 	{
 		name: 'NodeJs',
@@ -36,6 +35,22 @@ export default [
 			'Nodejs é runtime em Javascript que possíbilita execução de código a nível de backend e front-end.',
 	},
 	{
+		name: 'Git',
+		color: '#f04f30',
+		url: 'https://github.com/topics/git',
+		Icon: SiGit,
+		description:
+			'Git é o sistema de controle de versão mais usado, criado para o rastreamento e gerenciamento do código-fonte ao longo do tempo.',
+	},
+	{
+		name: 'Github',
+		color: '#FFFFFF',
+		url: 'https://github.com/',
+		Icon: IoLogoGithub,
+		description:
+			'Maior plataforma de hospedagem e colaboracão de código utilizando versionamento Git. ',
+	},
+	{
 		name: 'Javascript',
 		color: '#EFD81D',
 		url: 'https://github.com/topics/javascript',
@@ -44,12 +59,28 @@ export default [
 			'JavaScript é uma linguagem de programação que permite implementar funcionalidades mais complexas em páginas web.',
 	},
 	{
+		name: 'Typescript',
+		color: '#2f74c0',
+		url: 'https://github.com/topics/typescript',
+		Icon: SiTypescript,
+		description:
+			'TypeScript é uma linguagem de programação desenvolvida pela Microsoft, projetada para segurança de tipo ao JavaScript.',
+	},
+	{
 		name: 'Scss',
 		color: '#C66394',
 		url: 'https://github.com/topics/scss',
 		Icon: FaSass,
 		description:
 			'Sass(ou Scss) é a linguagem de extensão CSS de nível profissional mais madura, estável e poderosa do mundo.',
+	},
+	{
+		name: 'Less',
+		color: '#1d365d',
+		url: 'https://github.com/topics/less',
+		Icon: FaLess,
+		description:
+			'Less (que significa Leaner Style Sheets), como a Sass, é uma extensão de linguagem para CSS.',
 	},
 	{
 		name: 'Html5',
