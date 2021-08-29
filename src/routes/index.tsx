a0c226bcb423c2056b29bfa7621275eac081ca65
John Covv <johncovv@outlook.com>
2021-08-28 23:21:12 -0300

    Setting 404 redirect on global router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import ProjectDetails from '../pages/ProjectDetails';
@@ -9,6 +9,7 @@ const Routes: React.FunctionComponent = () => (
 	<Switch>
 		<Route path="/" exact component={Dashboard} />
 		<Route path="/project/:projectName" component={ProjectDetails} />
+		<Redirect from="*" to="/" />
 	</Switch>
 );
 
